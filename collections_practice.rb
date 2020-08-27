fa4fa1b959fdaf5eff75978e34b773d75643589f
Theodore Schafer <theodore.schafer@gmail.com>
2020-08-27 14:03:13 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,8 +15,8 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-
-
+  array[1], array[2] = array[2], array[1]
+  array
 end
 
 
@@ -25,13 +25,31 @@ def reverse_array(array)
 end
 
 def kesha_maker(array)
+  array.each do |i|
+    i[2] = "$"
+  end
 end
 
 def find_a(array)
+  array.find_all do |word|
+    word[0] == "a"
+  end
 end
 
 def sum_array(array)
+  sum = 0
+  array.each do |numbers|
+    sum+=numbers
+  end
+  return sum
 end
 
 def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
 end
