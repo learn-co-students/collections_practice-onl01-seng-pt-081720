91cfefb4832fed0965c619383ff8029445f4b07f
Anthony Hernandez <anthonyh2020@gmail.com>
2020-08-30 07:12:19 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,63 @@
+def sort_array_asc(integers)
+  integers.sort
+end  
+
+def sort_array_desc(integers)
+  integers.sort do |left, right|
+    right <=> left
+  end
+end
+
+def sort_array_char_count(strings)
+  strings.sort do |left, right|
+    left.length <=> right.length
+  end
+end
+
+def swap_elements(arrays)
+  arrays[1], arrays[2] = arrays[2], arrays[1]
+  arrays
+end
+
+def reverse_array(integers)
+  integers.reverse
+end
+
+def kesha_maker(strings)
+  strings.each do |string|
+    string[2] = "$"
+  end
+end
+
+def find_a(strings)
+  strings.find_all do |letter|
+    letter[0] == "a"
+  end
+end
+
+def sum_array(integers)
+  total = 0
+  integers.each do |integer|
+    total += integer
+  end
+  total
+end
+
+def add_s(arrays)
+  arrays.collect do |letter|
+    if arrays[1] == letter
+      letter
+    else
+      letter + "s"
+    end
+  end
+end
+
+
+
+
+
+
+
+
 
