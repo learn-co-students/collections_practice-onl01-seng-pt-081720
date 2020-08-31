85179172011eac5e4f19e302864f06f427942643
Myles Janik <mylesjanik@gmail.com>
2020-08-31 02:24:23 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -25,3 +25,12 @@ end
 def reverse_array(array)
   array.reverse
 end
+
+def kesha_maker(array)
+  new_array = []
+  new_name = ""
+  
+  array.each do |name|
+    new_name = name
+    new_name
+    new_array << 
